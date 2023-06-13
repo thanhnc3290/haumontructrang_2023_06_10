71783e2f9c2164e0ca8ef3d4140158c4f29cc73d
thanhnc3290 <crm.90jaran@gmail.com>
2023-06-13 13:55:26 +0700

    complete_html

--- assets/js/myjs.js
+++ assets/js/myjs.js
@@ -12,3 +12,27 @@ function tongle_main_menu(){
 function close_main_menu(){
     document.getElementById('main_menu').setAttribute('style','display:none;');
 }
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+}
